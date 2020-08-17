32b71ddc4c595588bf09aea8baa1641ea97e091a
alexperritaz <alexperritaz@gmail.com>
2020-08-17 18:20:46 +0200

    Update - Cleaned tmp files

--- transformers/fasttext.py
+++ transformers/fasttext.py
@@ -8,31 +8,7 @@ import lxml.etree as ET
 from utils import walk_files, progress_bar
 from sklearn.model_selection import train_test_split
 
-
-
-# def transform_XML(filepaths, labels, src_directory, dst_file):
-#     df_labels = pd.read_csv(src_labels)
-#     n_classes = df_labels['classes'].nunique()    
-#     documents = []
-#     fasttext = ''    
-#     for i in range(n_classes):
-#         classes_i = df_labels.loc[df_labels['classes'] == i]
-#         sorted_i = sorted(classes_i['nct_id'])
-#         for path in progress_bar(filepaths, "Extracting class %s : " % (i)):
-#             if any(nct_id in path for nct_id in sorted_i):
-#                 xmlstr = ET.tostring(ET.parse(path), encoding='utf-8', method='xml').decode('utf-8')
-#                 xmlstr = re.sub(' +', ' ', xmlstr)
-#                 xmlstr = xmlstr.replace('\n', '')
-#                 documents.append(fasttext + "__label__" + str(i) + " " + xmlstr + "\n")
-#         pass
-    
-#     # np.random.shuffle(documents)
-#     text_documents = ''.join(documents)
-#     with open(dst_file, "w", encoding='utf-8-sig') as txt_file:
-#         txt_file.write(text_documents)
-#     pass
-
-def transform_XML(filepaths, labels, src_directory, dst_file):
+def transform_XML(filepaths, labels,  dst_file):
     print(type(filepaths))
     documents = []
     fasttext = ''
@@ -53,16 +29,17 @@ def transform_XML(filepaths, labels, src_directory, dst_file):
 
 # random.seed(32)
 np.random.seed(69)
+
 src_directory = '../../data/interim/tags_few/sampled_20000_per_class/xml/'
 src_labels = '../../data/interim/tags_few/sampled_20000_per_class/labels_encoded.csv'
 filepaths = walk_files(src_directory)
-# np.random.shuffle(filepaths)
 
 data_x = pd.DataFrame(filepaths, columns=['filename']) 
 df_labels = pd.read_csv(src_labels)
 data_y = np.array(df_labels.to_numpy()[:,1:], dtype=np.float)
 
 X_trainval, X_test, y_trainval, y_test = train_test_split(data_x, data_y, test_size=0.2, stratify=data_y, random_state=69)
+X_train, X_val, y_train, y_val = train_test_split(X_trainval, y_trainval, test_size=0.25, stratify=y_trainval, random_state=21)
 
 # print(X_test[0:20])
 # print(y_test[0:20])
@@ -74,11 +51,9 @@ X_trainval, X_test, y_trainval, y_test = train_test_split(data_x, data_y, test_s
 # print(test[0])
 # print(y_test[0:20])
 
-
-# dst_file = '../../data/interim/tags_few/sampled_20000_per_class/fasttext_distr/fasttext_train.txt'
-# transform_XML(train, src_directory, src_labels, dst_file)
-# dst_file = '../../data/interim/tags_few/sampled_20000_per_class/fasttext_distr/fasttext_dev.txt'
-# transform_XML(dev, src_directory, src_labels, dst_file)
+dst_file = '../../data/interim/tags_few/sampled_20000_per_class/fasttext_distr/fasttext_train.txt'
+transform_XML(X_train, y_train, dst_file)
+dst_file = '../../data/interim/tags_few/sampled_20000_per_class/fasttext_distr/fasttext_dev.txt'
+transform_XML(X_val, y_val, dst_file)
 dst_file = '../../data/interim/tags_few/sampled_20000_per_class/fasttext_distr/fasttext_test.txt'
-transform_XML(X_test,y_test, src_directory, dst_file)
-# transform_XML(test, src_directory, src_labels, dst_file)
+transform_XML(X_test,y_test, dst_file)
